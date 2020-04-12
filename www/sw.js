211bd4efa075cef77b3bc0142a0c03b6db6adb22
Andi Baskoro <baskoro.ruliawan@gmail.com>
2020-04-12 19:08:21 +0700

    make tailwind local

--- www/sw.js
+++ www/sw.js
@@ -11,7 +11,7 @@
         './images/android-chrome-512x512.png',
         './images/favicon-16x16.png',
         './images/favicon-32x32.png',
-        'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css'
+        './tailwind.min.css'
     ];
 
     self.addEventListener('install', async function() {
